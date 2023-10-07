a04dec5d63dc61bc21d5fe001195fd983734cee1
musong <txz32102@gmail.com>
2023-10-07 20:57:57 +0800

    add alanysis

--- esm2_t6_8M_UR50D.py
+++ esm2_t6_8M_UR50D.py
@@ -244,7 +244,7 @@ def data_fit(train_df, test_df):
     X_test, y_test = shuffle(X_test, y_test, random_state=42)
     return X_train, y_train, X_test, y_test
 
-def main():
+def train_and_test():
     os.chdir('/home/musong/Desktop')
     df = pd.read_csv('/home/musong/Desktop/embedded_data_with_label.csv')   
     train_df, test_df = balanced_data(df)
@@ -259,3 +259,115 @@ def main():
     X_train = scaler.transform(X_train) # normalize X to 0-1 range 
     X_test = scaler.transform(X_test)
     return X_train, y_train, X_test, y_test
+
+def ESM_CNN(X_train, y_train, X_test, y_test):
+    from keras.layers import Input,InputLayer, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D,Conv1D
+    from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, AveragePooling1D, MaxPooling1D
+    from keras.models import Sequential,Model
+    from keras.optimizers import SGD
+    from keras.callbacks import ModelCheckpoint,LearningRateScheduler, EarlyStopping
+    import keras
+    from keras import backend as K
+    import tensorflow as tf
+    inputShape=(320,1)
+    input = Input(inputShape)
+    x = Conv1D(128,(3),strides = (1),name='layer_conv1',padding='same')(input)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = MaxPooling1D((2), name='MaxPool1',padding="same")(x)
+    x = Dropout(0.15)(x)
+    x = Conv1D(32,(3),strides = (1),name='layer_conv2',padding='same')(input)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = MaxPooling1D((2), name='MaxPool2',padding="same")(x)
+    x = Dropout(0.15)(x)
+    x = Flatten()(x)
+    x = Dense(64,activation = 'relu',name='fc1')(x)
+    x = Dropout(0.15)(x)
+    x = Dense(2,activation = 'softmax',name='fc2')(x)
+    model = Model(inputs = input,outputs = x,name='Predict')
+    # define SGD optimizer
+    momentum = 0.5
+
+    initial_learning_rate = 0.1
+    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate, decay_steps=10000, decay_rate=0.96, staircase=True
+    )
+
+    # Create an optimizer with the learning rate schedule
+    optimizer = tf.keras.optimizers.SGD(learning_rate=lr_schedule)
+
+    # compile the model
+    model.compile(loss='sparse_categorical_crossentropy',optimizer=optimizer, metrics=['accuracy'])
+    # learning deccay setting
+    import math
+    def step_decay(epoch): # gradually decrease the learning rate 
+        initial_lrate=0.1
+        drop=0.6
+        epochs_drop = 3.0
+        lrate= initial_lrate * math.pow(drop,    # math.pow base raised to a power
+            math.floor((1+epoch)/epochs_drop)) # math.floor Round numbers down to the nearest integer
+        return lrate
+    lrate = LearningRateScheduler(step_decay)
+
+    # early stop setting
+    early_stop = EarlyStopping(monitor='val_accuracy', patience = 40,restore_best_weights = True)
+
+    # summary the callbacks_list
+    callbacks_list = [ lrate , early_stop]
+
+    model_history = model.fit(X_train, y_train, validation_data=(X_test, y_test),
+                            epochs=200,callbacks=callbacks_list,batch_size = 8, verbose=1)
+    return model, model_history
+
+def train():
+    from sklearn.model_selection import KFold
+    k = 10 
+    kf = KFold(n_splits=k, shuffle = True, random_state=1)
+    X_train = pd.DataFrame(X_train)
+    y_train = pd.DataFrame(y_train)
+
+    # result collection list
+    ACC_collecton = []
+    BACC_collecton = []
+    Sn_collecton = []
+    Sp_collecton = []
+    MCC_collecton = []
+    AUC_collecton = []
+
+    for train_index , test_index in kf.split(y_train):
+        X_train_CV , X_valid_CV = X_train.iloc[train_index,:],X_train.iloc[test_index,:]
+        y_train_CV , y_valid_CV = y_train.iloc[train_index] , y_train.iloc[test_index]
+        model, model_history = ESM_CNN(X_train_CV, y_train_CV, X_valid_CV, y_valid_CV)
+        # confusion matrix 
+        predicted_class= []
+        predicted_protability = model.predict(X_valid_CV,batch_size=1)
+        for i in range(predicted_protability.shape[0]):
+            index = np.where(predicted_protability[i] == np.amax(predicted_protability[i]))[0][0]
+            predicted_class.append(index)
+        predicted_class = np.array(predicted_class)
+        y_true = y_valid_CV    
+        from sklearn.metrics import confusion_matrix
+        import math
+        # np.ravel() return a flatten 1D array
+        TP, FP, FN, TN = confusion_matrix(y_true, predicted_class).ravel() # shape [ [True-Positive, False-positive], [False-negative, True-negative] ]
+        ACC = (TP+TN)/(TP+TN+FP+FN)
+        ACC_collecton.append(ACC)
+        Sn_collecton.append(TP/(TP+FN))
+        Sp_collecton.append(TN/(TN+FP))
+        MCC = (TP*TN-FP*FN)/math.pow(((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN)),0.5)
+        MCC_collecton.append(MCC)
+        BACC_collecton.append(0.5*TP/(TP+FN)+0.5*TN/(TN+FP))
+        from sklearn.metrics import roc_auc_score
+        AUC = roc_auc_score(y_valid_CV, predicted_protability[:,1])
+        AUC_collecton.append(AUC)
+
+def show_result():
+    print(ACC_collecton[0])
+    print(BACC_collecton[0])
+    print(Sn_collecton[0])
+    print(Sp_collecton[0])
+    print(MCC_collecton[0])
+    print(AUC_collecton[0])
+    model.save('AHT_main_tensorflow_model',save_format = 'tf') 
+    !zip -r /content/AHT_main_tensorflow_model.zip /content/AHT_main_tensorflow_model
