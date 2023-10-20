8d026e8a1815034a88af6a060a142861bd889fec
musong <txz32102@gmail.com>
2023-10-20 16:57:15 +0800

    df file path changed

--- drugfinder/GaussianNB.py
+++ drugfinder/GaussianNB.py
@@ -1,41 +1,29 @@
-from sklearn.svm import SVC
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_curve, auc
-import matplotlib.pyplot as plt
-from sklearn.naive_bayes import GaussianNB
+import os
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import roc_curve, auc
+from sklearn.naive_bayes import GaussianNB
 import numpy as np
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('/home/musong/Desktop/paper/data/drugfinder/esm2_320_dimensions_with_labels.csv') 
+file_path = os.path.join(os.getcwd(), "paper/data/drugfinder/esm2_320_dimensions_with_labels.csv") 
+df = pd.read_csv(file_path) 
 
 X = df.drop(['label', 'UniProt_id'], axis=1)
 y = df['label'].apply(lambda x: 0 if x != 1 else x).to_numpy().astype(np.int64)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+scalar = MinMaxScaler()
+X_train = scalar.fit_transform(X_train)
+X_test = scalar.fit_transform(X_test)
 
-# Assuming you have loaded or created X_train, X_test, y_train, and y_test
-
-# Apply Min-Max scaling to your data
-scaler = MinMaxScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)  # Use transform on the test set, not fit_transform
-
-# Create a Gaussian Naive Bayes Classifier
 nb_classifier = GaussianNB()
-
-# Train the model on the training data
 nb_classifier.fit(X_train, y_train)
 
-# Make predictions on the test data
 y_score = nb_classifier.predict_proba(X_test)[:, 1]
-
-# Compute the ROC curve
 fpr, tpr, thresholds = roc_curve(y_test, y_score)
-
-# Calculate the AUC (Area Under the Curve)
 roc_auc = auc(fpr, tpr)
 
-# Plot the ROC curve
 plt.figure()
 plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (AUC = {roc_auc:.2f})')
 plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
