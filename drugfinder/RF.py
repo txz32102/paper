0af3ee6983fd71be07faf3cc4beb3978454871db
musong <txz32102@gmail.com>
2023-10-20 11:48:34 +0800

    ml methods

---
+++ drugfinder/RF.py
@@ -0,0 +1,59 @@
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+from sklearn.ensemble import RandomForestClassifier
+if os.path.exists('/content'):
+    if os.path.exists('/content/drive/MyDrive'):
+        os.chdir('/content/drive/MyDrive')
+        df = pd.read_csv('esm2_320_dimensions_with_labels.csv') 
+    else:
+        os.chdir('/home/musong/Desktop')
+        df = pd.read_csv('/home/musong/Desktop/paper/data/drugfinder/esm2_320_dimensions_with_labels.csv') 
+else:
+    os.chdir('/home/musong/Desktop')
+    df = pd.read_csv('/home/musong/Desktop/paper/data/drugfinder/esm2_320_dimensions_with_labels.csv') 
+
+X = df.drop(['label', 'UniProt_id'], axis=1)
+y = df['label'].apply(lambda x: 0 if x != 1 else x).to_numpy().astype(np.int64)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+scalar = MinMaxScaler()
+X_train = scalar.fit_transform(X_train)
+X_test = scalar.fit_transform(X_test)
+
+# Assuming you have loaded or created X_train, X_test, y_train, and y_test
+
+# Apply Min-Max scaling to your data
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)  # Use transform on the test set, not fit_transform
+
+# Create a Random Forest Classifier
+rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+
+# Train the model on the training data
+rf_classifier.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_score = rf_classifier.predict_proba(X_test)[:, 1]
+
+# Compute the ROC curve
+fpr, tpr, thresholds = roc_curve(y_test, y_score)
+
+# Calculate the AUC (Area Under the Curve)
+roc_auc = auc(fpr, tpr)
+
+# Plot the ROC curve
+plt.figure()
+plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (AUC = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc="lower right")
+plt.show()
