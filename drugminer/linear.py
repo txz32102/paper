8511a35df01c99933760bc5faa508ccc8efbdf8d
musong <txz32102@gmail.com>
2023-11-18 19:51:12 +0800

    drugfinder->drugminer

--- drugminer/linear.py
+++ drugminer/linear.py
@@ -1,20 +1,30 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import roc_curve, auc, matthews_corrcoef, f1_score, recall_score, accuracy_score
+from sklearn.metrics import (
+    roc_curve,
+    auc,
+    matthews_corrcoef,
+    f1_score,
+    recall_score,
+    accuracy_score,
+)
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
-import torch.nn as nn 
+import torch.nn as nn
 
-df = pd.read_csv('data/drugfinder/esm2_320_dimensions_with_labels.csv') 
-X = df.drop(['label', 'UniProt_id'], axis=1).values
-y = df['label'].apply(lambda x: 0 if x != 1 else x).values
+df = pd.read_csv("data/drugfinder/esm2_320_dimensions_with_labels.csv")
+X = df.drop(["label", "UniProt_id"], axis=1).values
+y = df["label"].apply(lambda x: 0 if x != 1 else x).values
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, shuffle=True)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 X_test = torch.tensor(X_test, dtype=torch.float32)
 y_test = torch.tensor(y_test, dtype=torch.float32).reshape(-1, 1)
+
 
 class Deep(nn.Module):
     def __init__(self):
@@ -34,9 +44,10 @@ class Deep(nn.Module):
         x = self.act3(self.layer3(x))
         x = self.sigmoid(self.output(x))
         return x
-    
+
+
 model = Deep()
-model.load_state_dict(torch.load('drugfinder/linear.pt'))
+model.load_state_dict(torch.load("drugminer/linear.pt"))
 with torch.no_grad():
     y = model(X_test).reshape(-1)
     y_predict = y.numpy()
@@ -56,12 +67,12 @@ print("F1 Score:", f1)
 print("Recall:", recall)
 
 plt.figure()
-plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (AUC = {roc_auc:.2f})')
-plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.plot(fpr, tpr, color="darkorange", lw=2, label=f"ROC curve (AUC = {roc_auc:.2f})")
+plt.plot([0, 1], [0, 1], color="navy", lw=2, linestyle="--")
 plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver Operating Characteristic')
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.title("Receiver Operating Characteristic")
 plt.legend(loc="lower right")
 plt.show()
