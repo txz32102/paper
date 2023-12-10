c282a2318bab446e9edb58d0b9359c7e4d447f3e
musong <txz32102@gmail.com>
2023-12-10 12:18:17 +0800

    argparser

--- util/preprocessing.py
+++ util/preprocessing.py
@@ -3,10 +3,13 @@ import esm
 import numpy as np
 import torch
 from tqdm import tqdm
+import argparse
 
 
-def esm_embeddings(peptide_sequence_list):
+def esm_embeddings(peptide_sequence_list, model_path = None):
     model, alphabet = esm.pretrained.esm2_t6_8M_UR50D()
+    if(model_path is not None):
+        model.state_dict(torch.load(model_path))
     batch_converter = alphabet.get_batch_converter()
     model.eval()  # disables dropout for deterministic results
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -96,32 +99,41 @@ def get_negative_peptide_sequence_list(file_path):
         peptide_sequence_list.append((current_identifier, current_sequence))
     return peptide_sequence_list
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Process peptide sequences.')
+    parser.add_argument('--negative_test_path', type=str, required=False, default='data/drugminer/fastadata/Independent_Test/negative_test_sequence.fasta',
+                        help='Path to the negative test sequence FASTA file')
+    parser.add_argument('--positive_test_path', type=str, required=False, default='data/drugminer/fastadata/Independent_Test/positive_test_sequence.fasta',
+                        help='Path to the positive test sequence FASTA file')
+    parser.add_argument('--negative_train_path', type=str, required=False, default='data/drugminer/fastadata/Train/negative_train_sequence.fasta',
+                        help='Path to the negative train sequence FASTA file')
+    parser.add_argument('--positive_train_path', type=str, required=False, default='data/drugminer/fastadata/Train/positive_train_sequence.fasta',
+                        help='Path to the positive train sequence FASTA file')
+    parser.add_argument('--output_path', type=str, required=False, default='data/drugminer/esm2_320_dimensions_with_labels.csv',
+                        help='Path for output CSV file')
+    parser.add_argument('--model_path', type=str, required=False, default=None,
+                        help='if one use the fine-tuning model or you can use the model trained from scratch')
+    return parser.parse_args()
+
 
 def main():
+    args = parse_arguments()
     columns = ["UniProt_id", "sequence", "label", "train or test from original data"]
     df = pd.DataFrame(columns=columns)
-    negative_test_path = (
-        "data/drugminer/fastadata/Independent_Test/negative_test_sequence.fasta"
-    )
-    positive_test_path = (
-        "data/drugminer/fastadata/Independent_Test/positive_test_sequence.fasta"
-    )
-    negative_train_path = "data/drugminer/fastadata/Train/negative_train_sequence.fasta"
-    positive_train_path = "data/drugminer/fastadata/Train/positive_train_sequence.fasta"
 
-    negative_test = get_negative_peptide_sequence_list(negative_test_path)
+    negative_test = get_negative_peptide_sequence_list(args.negative_test_path)
     for id, sequence in negative_test:
         row = [id, sequence, 0, "test"]
         df.loc[len(df)] = row
-    negative_train = get_negative_peptide_sequence_list(negative_train_path)
+    negative_train = get_negative_peptide_sequence_list(args.negative_train_path)
     for id, sequence in negative_train:
         row = [id, sequence, 0, "train"]
         df.loc[len(df)] = row
-    positive_train = get_positive_peptide_sequence_list(positive_train_path)
+    positive_train = get_positive_peptide_sequence_list(args.positive_train_path)
     for id, sequence in positive_train:
         row = [id, sequence, 1, "train"]
         df.loc[len(df)] = row
-    positive_test = get_positive_peptide_sequence_list(positive_test_path)
+    positive_test = get_positive_peptide_sequence_list(args.positive_test_path)
     for id, sequence in positive_test:
         row = [id, sequence, 1, "test"]
         df.loc[len(df)] = row
@@ -135,7 +147,7 @@ def main():
     result_df = pd.DataFrame(columns=column_headers)
     for i in range(0, len(sorted_df)):
         row = sorted_df.iloc[i]
-        embeddings = esm_embeddings([(row["UniProt_id"], row["sequence"])])
+        embeddings = esm_embeddings([(row["UniProt_id"], row["sequence"])], model_path=args.model_path)
         data = (
             [embeddings[0][0]]
             + embeddings[0][1].cpu().numpy().tolist()
@@ -146,7 +158,8 @@ def main():
         if i % 100 == 0:
             tqdm.write(f"Processed {i+1} rows")
 
-    result_df.to_csv("data/drugminer/esm2_320_dimensions_with_labels.csv", index=False)
+    result_df.to_csv(args.output_path, index=False)
 
 
-main()
+if __name__ == "__main__":
+    main()
