c9926666a430214fec38e6103ff9763b383b960f
musong <txz32102@gmail.com>
2023-12-12 20:59:07 +0800

    error handling

--- util/pssm_1200d.py
+++ util/pssm_1200d.py
@@ -1,18 +1,28 @@
 from pssm import *
 import pandas as pd
 import os
+
+
 # you should place the pssm files in a folder, then it will output the 1200 dimensional matrix
 def pssm_1200d(pssm_path, output_csv):
     data = []
-    header = ['id', 'label'] + list(range(1, 1201))
+    header = ["id", "label"] + list(range(1, 1201))
     for file_name in os.listdir(pssm_path):
         if os.path.isfile(os.path.join(pssm_path, file_name)):
             file_path = os.path.join(pssm_path, file_name)
             pssm_data = read_pssm_matrix(file_path)
-            dpc_pssm_400 = dpc_pssm(pssm_data)
-            k_separated_bigrams_pssm_400 = k_separated_bigrams_pssm(pssm_data)
-            s_fpssm_400 = s_fpssm(pssm_data)
-            combined_features = np.concatenate((dpc_pssm_400, k_separated_bigrams_pssm_400, s_fpssm_400), axis=None)
+
+            try:
+                dpc_pssm_400 = dpc_pssm(pssm_data)
+                k_separated_bigrams_pssm_400 = k_separated_bigrams_pssm(pssm_data)
+                s_fpssm_400 = s_fpssm(pssm_data)
+            except Exception as e:
+                print(f"Error processing file {file_name}: {e}")
+                continue
+
+            combined_features = np.concatenate(
+                (dpc_pssm_400, k_separated_bigrams_pssm_400, s_fpssm_400), axis=None
+            )
             row = [file_name] + [0] + list(combined_features)
             data.append(row)
 
